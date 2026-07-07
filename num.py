d465e16aac35a48edcf3de72e78912122dcf6f55
YashviTala <yashvitala@gmail.com>
2026-07-07 08:33:48 +0530

    Add files via upload

---
+++ num.py
@@ -0,0 +1,14 @@
+import numpy as np
+arr=np.array([10,2,3])
+print(arr)
+
+arr=np.array(['x','y','z'])
+print(arr)
+
+arr=np.array([23.4,45.8,45.9])
+print(arr)
+
+num1=[1,2,3]
+num2=[1,2,3]
+arr=np.array([num1+num2])
+print(arr)
