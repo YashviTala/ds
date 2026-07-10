b1a481f37db1b1e4193dfdd3a40a27874d807549
YashviTala <yashvitala@gmail.com>
2026-07-10 09:09:50 +0530

    Add files via upload

---
+++ num 2.py
@@ -0,0 +1,12 @@
+import sys
+import numpy as np
+arr1=np.array([1,2,3])
+arr2=np.array([1,2,3])
+a=np.array([arr1+arr2])
+b=np.array([arr1-arr2])
+c=np.array([arr1*arr2])
+d=np.array([arr1/arr2])
+print(a)
+print(b)
+print(c)
+print(d)                            
